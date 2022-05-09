620dbf3d33c6bd8ad2709dce1ea9a1fb45bb64a6
Alvin <quhao317@163.com>
2022-05-10 00:22:47 +0800

    [basic] file lock demo

--- basic/concurrence/file_lock.py
+++ basic/concurrence/file_lock.py
@@ -1,8 +1,6 @@
 import fcntl
 import os
-from typing import Optional
-
-FileDescriptor = int
+from threading import get_ident
 
 
 class FileLock:
@@ -10,37 +8,7 @@ class FileLock:
     文件锁类型
     """
     # 锁文件名模板
-    _lock_file_template = "{}.lock"
-
-    class _Lock:
-        """
-        锁上下文类型
-        """
-
-        def __init__(self, fd: FileDescriptor) -> None:
-            """
-            初始化锁上下文对象
-
-            Args:
-                fd (FileDescriptor): 锁相关联的文件描述符对象
-            """
-            self._fd = fd
-
-        def __enter__(self) -> None:
-            """
-            进入锁上下文
-            """
-            pass
-
-        def __exit__(self, exc_type, exc_val, exc_tb) -> None:
-            """
-            退出锁上下文
-            """
-            if self._fd:
-                # 解除锁定
-                fcntl.flock(self._fd, fcntl.LOCK_UN)
-                # 清理文件描述符对象, 令锁无效
-                self._fd = None
+    _lock_file_template = ".{}.lock"
 
     def __init__(self, name: str) -> None:
         """
@@ -57,46 +25,62 @@ class FileLock:
 
         # 格式化锁文件名
         self._filename = self._lock_file_template.format(name)
+        # 文件描述符
+        self._fd = 0
+        # 是否锁定
+        self._locked = False
 
-    def lock(self) -> _Lock:
+    def acquire(self, blocking=True) -> bool:
         """
         加锁
+
         如果锁已经被占用则进入阻塞等待, 直到锁被释放
 
+        Args:
+            blocking (bool, optional): 是否阻塞. Defaults to False.
+
         Returns:
-            _Lock: 锁上下文对象
+            bool: 加锁成功返回 True
         """
         # 创建锁文件, 得到文件描述符
         fd = os.open(self._filename, os.O_RDONLY | os.O_CREAT)
 
-        # 对文件进行加锁操作
-        fcntl.flock(fd, fcntl.LOCK_EX)
-        # 返回锁上下文对象
-        return self._Lock(fd)
+        # 文件锁标识
+        opt = fcntl.LOCK_EX
+        if not blocking:
+            # 非阻塞标识
+            opt |= fcntl.LOCK_NB
 
-    def try_lock(self) -> Optional[_Lock]:
-        """
-        尝试加锁
-        如果锁已经被占用则返回 None
-
-        Returns:
-            Optional[_Lock]: 如果加锁成功则返回锁上下文对象, 否则返回 None
-        """
-        # 创建锁文件, 得到文件描述符
-        fd = os.open(self._filename, os.O_RDONLY | os.O_CREAT)
         try:
-            # 对文件进行加锁操作, LOCK_NB 表示不阻塞
-            fcntl.flock(fd, fcntl.LOCK_EX | fcntl.LOCK_NB)
-            return self._Lock(fd)
-        except BlockingIOError:
-            # 如果抛出 BlockingIOError, 表示锁已被占用, 返回 None 值
-            return None
+            # 对文件进行加锁操作
+            fcntl.flock(fd, opt)
+            # 保存文件描述符
+            self._fd = fd
 
-    def remove(self) -> None:
+            # 返回加锁成功
+            return True
+        except Exception:
+            # 返回加锁失败
+            return False
+
+    # 定义 __enter__ 方法
+    __enter__ = acquire
+
+    def release(self) -> None:
         """
-        删除锁文件
+        解锁
         """
+        fd = self._fd
+        if fd:
+            fcntl.flock(fd, fcntl.LOCK_UN)
+            os.close(fd)
+
+            self._fd = None
+
         try:
             os.remove(self._filename)
         except Exception:
             pass
+
+    def __exit__(self, t, v, tb) -> None:
+        self.release()
--- basic/concurrence/test_file_lock.py
+++ basic/concurrence/test_file_lock.py
@@ -1,52 +1,92 @@
 import time
+import timeit
 from itertools import repeat
+from multiprocessing import cpu_count
 from multiprocessing.pool import ThreadPool
-from typing import List, Optional
+from typing import Any, Callable, Iterator, Optional
 
 from .file_lock import FileLock
 
 
-def work(id_: str, results: List[str], lock_name: Optional[str]) -> None:
-    """_summary_
+def run_worker_by_pool(
+    worker: Callable[[Iterator[Any]], None],
+    args: Iterator[Any]
+) -> float:
+    """
+    通过线程池执行异步函数
 
     Args:
-        id_ (str): _description_
-        results (List[str]): _description_
-        lock (Optional[FileLock]): _description_
+        worker (Callable[[Iterator[Any]], None]): 异步函数对象
+        args (Iterator[Any]): 异步函数参数, 参数个数为执行异步函数的线程数
+
+    Returns:
+        float: 整体耗时数
     """
-    def job() -> None:
+    start = timeit.default_timer()
+    with ThreadPool(cpu_count() * 2) as pool:
+        pool.map(worker, args)
+
+    return round(timeit.default_timer() - start, 1)
+
+
+def test_file_lock_blocked() -> None:
+    """
+    测试阻塞方式的文件锁
+    """
+
+    def worker(lock_name: Optional[str]) -> None:
         """
-        执行具体任务, 输出结果
+        线程入口函数, 执行一次耗时 0.1 秒
+
+        Args:
+            lock_name (Optional[str]): 锁文件名称, None 表示不加锁
         """
-        for i in range(2):
-            results.append(f"{id_}_{i}")
-            time.sleep(0.001)
+        if lock_name:
+            # 加锁
+            with FileLock(lock_name):
+                time.sleep(0.1)
+        else:
+            # 不加锁
+            time.sleep(0.1)
 
-    if lock_name:
-        lock = FileLock(lock_name)
-        try:
-            with lock.lock():
-                job()
-        finally:
-            lock.remove()
-    else:
-        job()
+    # 通过 5 个线程执行, 不加锁
+    r = run_worker_by_pool(worker, repeat(None, 5))
+    assert 0.1 <= r < 0.2  # 整体执行耗时约 0.1 秒, 表示 5 个线程并发同时执行
+
+    # 通过 5 个线程执行, 加锁
+    r = run_worker_by_pool(worker, repeat("lock", 5))
+    assert 0.5 <= r < 0.6  # 整体执行耗时约 0.5 秒, 表示 5 个线程依次顺序执行
 
 
-def test_file_lock() -> None:
-    def run(lock_names: Optional[List[str]]) -> List[str]:
-        results = []
-        with ThreadPool(2) as pool:
-            args = zip(["A", "B"], repeat(results), lock_names)
-            pool.starmap(work, args)
+def test_file_lock_non_blocked() -> None:
+    """
+    测试非阻塞方式加锁
+    """
 
-        return results
+    def worker(lock_name: Optional[str]) -> None:
+        """
+        线程入口函数, 执行一次耗时 0.1 秒
 
-    r = run(repeat(None))
-    assert r == ["A_0", "B_0", "A_1", "B_1"]
+        Args:
+            lock_name (Optional[str]): 锁文件名称, None 表示不加锁
+        """
+        if lock_name:
+            # 加锁
+            fl = FileLock(lock_name)
+            # 非阻塞方式加锁
+            if fl.acquire(blocking=False):
+                try:
+                    time.sleep(0.1)
+                finally:
+                    fl.release()
+        else:
+            # 不加锁
+            time.sleep(0.1)
 
-    r = run(repeat("lock"))
-    assert r == ["B_0", "B_1", "A_0", "A_1"]
+    # 通过 5 个线程执行, 不加锁
+    r = run_worker_by_pool(worker, repeat(None, 5))
+    assert 0.1 <= r < 0.2  # 整体执行耗时约 0.1 秒, 表示 5 个线程并发同时执行
 
-    r = run(["lock1", "lock2"])
-    assert r == ["A_0", "B_0", "A_1", "B_1"]
+    # 通过 5 个线程执行, 加锁
+    r = run_worker_by_pool(worker, repeat("lock", 5))
+    assert 0.1 <= r < 0.2  # 整体执行耗时约 0.1 秒, 表示 5 个线程并发同时执行, 加锁不阻塞执行
