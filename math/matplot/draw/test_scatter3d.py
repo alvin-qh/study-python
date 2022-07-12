9ef1d264142a5ce208e2ad7c9bd51854dc63130b
Alvin <quhao317@163.com>
2022-07-12 16:01:30 +0800

    [math] add matplot demo

--- math/matplot/draw/test_scatter3d.py
+++ math/matplot/draw/test_scatter3d.py
@@ -1,7 +1,7 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-from ..common import render_axis
+from ..common import render_axis3d, get_axis3d
 
 
 def test_draw_splashes() -> None:
@@ -11,31 +11,27 @@ def test_draw_splashes() -> None:
     通过一组圆心值绘制实心圆圈, 通过 `c` (`color`) 参数指定颜色
     """
     # 绘制坐标系和网格
-    render_axis(
-        (0, 20),  # x 坐标轴的范围
-        (70, 120),  # y 坐标轴的范围
-        grid=(5, 10),  # 网格大小
+    render_axis3d(
+        (-10, 10),  # x 坐标轴的范围
+        (-10, 10),  # y 坐标轴的范围
+        (-10, 10),  # z 坐标轴的范围
+        grid=(2, 2, 2),  # 网格大小
         axis=False,
     )
 
-    # 第 1 组圆心的 x 轴坐标集合
-    x = np.array([5, 7, 8, 7, 2, 17, 2, 9, 4, 11, 12, 9, 6])
-    # 第 1 组圆心的 y 轴坐标集合
-    y = np.array([99, 86, 87, 88, 111, 86, 103, 87, 94, 78, 77, 85, 86])
+    ax = get_axis3d()
 
-    # 绘制第 1 组点
-    plt.scatter(x, y, c="hotpink", marker="o")
+    x1 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+    y1 = [5, 6, 7, 8, 2, 5, 6, 3, 7, 2]
+    z1 = [1, 2, 6, 3, 2, 7, 3, 3, 7, 2]
 
-    # 第 2 组圆心的 x 轴坐标集合
-    x = np.array([2, 2, 8, 1, 15, 8, 12, 9, 7, 3, 11, 4, 7, 14, 12])
-    # 第 2 组圆心的 y 轴坐标集合
-    y = np.array([
-        100, 105, 84, 105, 90, 99, 90, 95, 94, 100, 79, 112, 91, 80, 85
-    ])
-    # 绘制第 2 组点
-    plt.scatter(x, y, c='#88c999', marker="x")
+    x2 = [-1, -2, -3, -4, -5, -6, -7, -8, -9, -10]
+    y2 = [-5, -6, -7, -8, -2, -5, -6, -3, -7, -2]
+    z2 = [1, 2, 6, 3, 2, 7, 3, 3, 7, 2]
 
-    # 显示画布
+    ax.scatter(x1, y1, z1, c="g", marker="o")
+    ax.scatter(x2, y2, z2, c="r", marker="x")
+
     plt.show()
 
 
@@ -50,29 +46,34 @@ def test_draw_bubbles() -> None:
     # 产生 50 个坐标
     x = np.random.rand(N)  # x 坐标轴上 50 个随机值
     y = np.random.rand(N)  # y 坐标轴上 50 个随机值
+    z = np.random.rand(N)  # z 坐标轴上 50 个随机值
 
     # 绘制坐标系和网格
-    render_axis(
+    render_axis3d(
         (-0.1, 1.1),  # x 坐标轴的范围
         (-0.1, 1.1),  # y 坐标轴的范围
-        grid=(0.1, 0.1),  # 网格大小
+        (-0.1, 1.1),  # z 坐标轴的范围
+        grid=(0.1, 0.1, 0.1),  # 网格大小
         axis=False,
     )
+
+    ax = get_axis3d()
+
 
     # 产生 50 个颜色值
     c = np.random.rand(N)
 
-    # 产生 50 个随机半径 (0 ~ 15) 并计算对应的面积
+    # 产生 50 个随机半径 (0 ~ 15) 并计算对应的大小
     area = np.pi * (15 * np.random.rand(N)) ** 2
 
-    plt.scatter(
+    ax.scatter(
         x,  # 点在 x 轴的坐标集
         y,  # 点在 y 轴的坐标集
-        s=area,  # 对应的圆面积
+        z,  # 点在 z 轴的坐标集
+        s=area,  # 对应的圆大小, 默认 20
         c=c,  # type: ignore # 对应的颜色, 这里设为调色板的颜色编号, 0~100 之间
         cmap="afmhot_r",  # 设置调色板, 之后可以通过 0~100 的数值代表调色板的颜色
-        alpha=0.5,
     )
 
-    plt.colorbar()  # type: ignore # 显示调色板, 即各数字对应的颜色
+    # plt.colorbar()  # type: ignore # 显示调色板, 即各数字对应的颜色
     plt.show()
