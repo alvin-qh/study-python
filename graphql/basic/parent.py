02457d0fa717806a3e857543e7ca8516e84b6c2d
Alvin <quhao317@163.com>
2022-07-20 04:48:51 +0800

    [graphql] add demo intro

--- graphql/basic/parent.py
+++ graphql/basic/parent.py
@@ -1,6 +1,40 @@
-from typing import Literal
+"""
+`parent` 参数
 
-from graphene import Argument, Field, ObjectType, ResolveInfo, Schema, String
+Graphene 中的各类方法, 往往要传递一个 `parent` 参数 (例如: resolve 方法或 mutation 方法)
+
+`parent` 参数表示 "上一级解析对象", 即:
+
+当查询顶级字段时 (即定义在 Query 类型中的字段), 第一级解析对象的 `parent` 参数为 `None` 或
+从 `schema.execute` 函数的 `root` 参数传递
+
+当解析某字段的下级字段时, `parent` 参数表示的为该查询字段的上一级字段实例, 例如:
+
+```python
+class Foo(ObjectType):
+    name = String(required=True)
+
+    @staticmethod
+    def resolve_name(parent: "Foo", info: ResolveInfo) -> str:
+        ...
+```
+
+这段代码的含义是: 当要解析 `name` 字段时, 一定存在上一级对 `foo` 字段的解析, 即:
+
+```python
+class Query(ObjectType):
+    foo = Field(Foo, required=True)
+
+    def resolve_foo(parent: Literal[None], info: ResolveInfo) -> Foo:
+        ...
+```
+
+所以 `resolve_foo` 返回的对象将作为 `resolve_name` 的 `parent` 参数
+"""
+
+from typing import Optional
+
+from graphene import Field, ObjectType, ResolveInfo, Schema, String
 
 
 class Person(ObjectType):
@@ -22,13 +56,10 @@ class Person(ObjectType):
     # 第二个名字
     last_name = String(required=True)
     # 全名
-    full_name = String(
-        splitter=Argument(String, default_value="·"),  # 全名中间的分割符
-        required=True,
-    )
+    full_name = String(required=True)
 
     @staticmethod
-    def resolve_full_name(parent: "Person", info: ResolveInfo, splitter: str) -> str:
+    def resolve_full_name(parent: "Person", info: ResolveInfo) -> str:
         """
         解析 `full_name` 字段
 
@@ -39,7 +70,7 @@ class Person(ObjectType):
         Returns:
             str: 返回全名
         """
-        return f"{parent.first_name}{splitter}{parent.last_name}"
+        return f"{parent.first_name}·{parent.last_name}"
 
 
 class Query(ObjectType):
@@ -57,16 +88,24 @@ class Query(ObjectType):
     person = Field(Person, required=True)
 
     @staticmethod
-    def resolve_person(parent: Literal[None], info: ResolveInfo) -> Person:
+    def resolve_person(parent: Optional[str], info: ResolveInfo) -> Person:
         """
         解析 `person` 字段
 
         Returns:
+            parent: `schema.execute` 函数的 `root` 参数值
             Person: 返回查询的 `Person` 实体对象
         """
+
+        if (parent or "").lower() == "alvin":
+            return Person(
+                first_name="Alvin",
+                last_name="Qu",
+            )
+
         return Person(
-            first_name="Alvin",
-            last_name="Qu",
+            first_name="Emma",
+            last_name="Yua",
         )
 
 
