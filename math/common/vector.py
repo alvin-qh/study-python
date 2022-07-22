c7204bd9aa85ecdcfc9d1d78dbd6a197f5339698
AlvinQ <hqu@hipacloud.com>
2022-07-22 12:47:13 +0800

    [math] add vector 3d demo

--- math/common/vector.py
+++ math/common/vector.py
@@ -1,57 +1,59 @@
 from math import atan2, cos, pi, sin, sqrt
 from typing import Iterable, List
 
-from . import Number, PolarVector, Vector2D
+from . import Number, PolarVector, Vector, Vector2D, Vector3D
 
 
-def length(v: Vector2D) -> Number:
+def length(v: Vector) -> float:
     """
-    计算一个二维向量的长度
+    计算一个 N 维向量的长度
 
     Args:
-        v (Vector2D): 一个向量
+        v (Vector): 一个 N 维向量
 
     Returns:
-        Number: 向量长度
+        float: 向量长度
     """
-    return sqrt(v[0]**2 + v[1]**2)
+    return sqrt(sum(coord**2 for coord in v))
 
 
-def add(*vectors: Vector2D) -> Vector2D:
+def add(*vs: Vector) -> Vector:
     """
-    将一个二维向量集合中的所有向量进行相加后返回结果
+    将一个 N 维向量集合中的所有向量进行相加后返回结果
 
     Args:
-        vectors (Iterable[Vector2D]): 向量集合
+        vs (Iterable[Vector]): 向量集合
 
     Returns:
-        Vector2D: 所有向量相加后的结果
+        Vector: 所有向量相加后的结果
     """
-    x: Number
-    y: Number
+    # 获取向量的维数
+    n = len(vs[0])
 
-    x, y = 0, 0
-    for v in vectors:
-        x += v[0]
-        y += v[1]
+    # 初始化记录结果的向量
+    r = [0.] * n
 
-    return (x, y)
+    for v in vs:
+        for i in range(n):
+            r[i] += v[i]
+
+    return tuple(r)
 
 
-def translate(offset: Vector2D, vectors: Iterable[Vector2D]) -> List[Vector2D]:
+def translate(v_off: Vector, vs: Iterable[Vector]) -> List[Vector]:
     """
     移动一个向量集合中的每个向量
 
     最终, 向量组成的形态不变, 但向量的位置会发生变化
 
     Args:
-        offset (Vector2D): 向量偏移量
-        vectors (Iterable[Vector2D]): 向量集合
+        v_off (Vector): 向量偏移量
+        vs (Iterable[Vector]): 向量集合
 
     Returns:
-        List[Vector2D]: 移动位置后的向量集合
+        List[Vector]: 移动位置后的向量集合
     """
-    return [add(v, offset) for v in vectors]
+    return [add(v, v_off) for v in vs]
 
 
 # 定义 1° 角度对应的弧度
@@ -109,10 +111,10 @@ def to_polar(v: Vector2D) -> PolarVector:
     将笛卡尔坐标向量转换为极坐标向量
 
     Args:
-        v (Vector2D): 笛卡尔坐标向量
+        v (Vector2D): 二维向量的笛卡尔坐标
 
     Returns:
-        PolarVector: 极坐标向量
+        PolarVector: 二维向量的极坐标
     """
     # 获取笛卡尔坐标向量的分量
     x, y = v[0], v[1]
@@ -120,3 +122,16 @@ def to_polar(v: Vector2D) -> PolarVector:
     angle = atan2(y, x)
     # 利用 length 函数求向量的长度, 返回极坐标向量
     return (length(v), angle)
+
+
+def scale(v: Vector, scalar: Number) -> Vector:
+    """
+    二维向量和标量相乘
+
+    Args:
+        v (Vector): N 维向量
+
+    Returns:
+        Vector: 缩放后的向量
+    """
+    return tuple(coord * scalar for coord in v)
