da61883b13208a92f4ba9141d2b6bad120c4e74f
Alvin <quhao317@163.com>
2022-08-29 23:52:21 +0800

    fix code styles

--- basic/testing/hypothesis/test_composite_and_drawing_interactively.py
+++ basic/testing/hypothesis/test_composite_and_drawing_interactively.py
@@ -1,4 +1,4 @@
-from typing import Any, List, Tuple, TypeVar
+from typing import List, Tuple, TypeVar
 
 from hypothesis import Verbosity, given, note, settings
 from hypothesis import strategies as st
