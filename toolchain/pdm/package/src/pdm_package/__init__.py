19e028e95b6fb2a34369c75cd243026f2f9a7269
AlvinQ <quhao317@163.com>
2025-05-06 18:28:08 +0800

    [toolchain/pdm] add README.md document

---
+++ toolchain/pdm/package/src/pdm_package/__init__.py
@@ -0,0 +1,26 @@
+from pdm_lib import add
+
+
+def run() -> None:
+    """主函数"""
+
+    # 从标准输入中读取两个值
+    print("Please input first value: ", end="")
+    i1 = input()
+
+    print("Please input second value: ", end="")
+    i2 = input()
+
+    # 从标准输入中读取值类型
+    print("Please select value type [(n)umber/(s)tring]: ", end="")
+    t = input()
+
+    # 计算结果并输出到标准输出中
+    match (t):
+        case "n":
+            print(f"Result: {add(int(i1), int(i2))}")
+        case "s":
+            print(f"Result: {add(i1, i2)}")
+        case _:
+            print("Invalid value type")
+            exit(1)
--- toolchain/pdm/package/tests/pdm_package/test_pdm_package.py
+++ toolchain/pdm/package/tests/pdm_package/test_pdm_package.py
@@ -1,8 +1,8 @@
 from pdm_package.lib import stdin_redirected, stdout_redirected
-from pdm_package.main import main
+from pdm_package import run
 
 
-def test_main() -> None:
+def test_run() -> None:
     """测试 `pdm_package.main.py` 下的 `main` 函数"""
     # 重定向标准输入, 依次输入 `1`, `2`, `n` 三个字符串
     with stdin_redirected("1\n2\nn\n"):
@@ -10,8 +10,8 @@ def test_main() -> None:
         # 重定向标准输出到一个 `StringIO` 对象
         with stdout_redirected() as s_io:
 
-            # 执行 `main` 函数
-            main()
+            # 执行 `run` 函数
+            run()
 
             # 确认标准输出的内容符合预期
             assert s_io.getvalue() == (
