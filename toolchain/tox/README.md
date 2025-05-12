bea9683653bebbe236e17d51661a09c8ba8a2869
AlvinQ <quhao317@163.com>
2025-05-12 17:56:49 +0800

    [toolchain/tox] update demo

---
+++ toolchain/tox/README.md
@@ -0,0 +1,13 @@
+# Tox
+
+Tox 是一个 Python 上的测试管理框架, 可以按分类执行多组测试, 包括
+
+## 1. 安装
+
+Tox
+
+```bash
+pip install tox
+```
+
+## 2.
---
+++ toolchain/tox/lib/__init__.py
@@ -0,0 +1,8 @@
+from lib.math import add
+from lib.mem_io import stdin_redirected, stdout_redirected
+
+__all__ = [
+    "add",
+    "stdin_redirected",
+    "stdout_redirected",
+]
