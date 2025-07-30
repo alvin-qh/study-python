d8aa6deb720121742f255f52523b43dbe4ea646c
AlvinQ <quhao317@163.com>
2025-07-30 13:36:00 +0800

    [algorithms/numpy] add demos

--- algorithms/numpy/lib/array_/__init__.py
+++ algorithms/numpy/lib/array_/__init__.py
@@ -1,5 +1,5 @@
 import numpy as np
 
 
-def arange_by_shape(start: int, shape: tuple[int, ...], step: int = 1) -> np.ndarray:
+def arange_by_shape(shape: tuple[int, ...], start: int, step: int = 1) -> np.ndarray:
     return np.arange(start, start + np.prod(shape) * step, step).reshape(shape)
