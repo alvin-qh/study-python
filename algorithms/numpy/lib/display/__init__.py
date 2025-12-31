357261da70fee389d24cb175604f39c6aa9fb9fa
Alvin <quhao317@163.com>
2026-01-01 01:52:56 +0800

    [algorithms/numpy] update demo

--- algorithms/numpy/lib/display/__init__.py
+++ algorithms/numpy/lib/display/__init__.py
@@ -3,21 +3,23 @@ from typing import Any
 import numpy as np
 
 
-def aprint(propmpt: str, /, values: dict[str, Any]) -> None:
+def aprint(propmpt: str, /, values: dict[str, Any], show_shapes: bool = True) -> None:
     """打印提示信息和字典格式的值, 对 numpy 数组和矩阵额外显示形状信息
 
     Args:
         `propmpt` (`str`): 要打印的提示信息
         `values` (`dict[str, Any]`): 包含要打印的键值对的字典
-
+        `show_shapes` (`bool`, optional): 是否显示数组的形状, 默认为 `True`
     Returns:
         `None`
     """
     print(f"{propmpt}")
+    prefix = "▶️"
     for key, value in values.items():
-        print(f"> {key}:")
+        print(f"{prefix} {key}:")
         print(f"{value}", end="")
-        if isinstance(value, (np.ndarray, np.matrix)):
+        if isinstance(value, (np.ndarray, np.matrix)) and show_shapes:
             print(f", shape={value.shape}")
         else:
             print()
+        prefix = "✅"
